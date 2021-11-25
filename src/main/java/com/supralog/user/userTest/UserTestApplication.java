8ac2ff2d7599f51085c952a36a6312c99fd9dc49
Fabien Anelli <fabien.anelli@yahoo.fr>
2021-11-25 21:48:43 +0100

    first commit

---
+++ src/main/java/com/supralog/user/userTest/UserTestApplication.java
@@ -0,0 +1,13 @@
+package com.supralog.user.userTest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserTestApplication.class, args);
+	}
+
+}
